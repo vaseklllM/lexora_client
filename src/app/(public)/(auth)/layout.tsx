d09d71a7d31d24a6b8c5b615ca4b0eea10b73008
Vasyl M. <vasia.motnin@gmail.com>
2025-09-10 21:31:10 +0200

    Add auth background image to layout and adjust layout structure for improved responsiveness

--- src/app/(public)/(auth)/layout.tsx
+++ src/app/(public)/(auth)/layout.tsx
@@ -6,15 +6,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <Image
-        alt="Your Company"
-        src="/logo.svg"
-        className="mx-auto h-20 w-auto"
-        width={100}
-        height={100}
-      />
-      {children}
+    <div className="flex h-screen justify-center">
+      <div className="flex w-2/5 flex-col items-center justify-center">
+        <Image
+          alt="Your Company"
+          src="/logo.svg"
+          className="mx-auto h-20 w-auto"
+          width={100}
+          height={100}
+        />
+        {children}
+      </div>
+      <div className="flex w-3/5 flex-col items-center justify-center">
+        <Image
+          src="/auth_background.avif"
+          alt="auth_background"
+          width={1000}
+          height={1000}
+          className="h-full w-full object-cover"
+        />
+      </div>
     </div>
   );
 }

7e8b1256abddabea39b4fdd7609812e8e157866f
Vasyl M. <vasia.motnin@gmail.com>
2025-09-13 18:12:03 +0200

    Add page component for authentication handling; redirect users to sign-in or dashboard based on authentication status.

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { checkIsAuth } from "@/shared/api/core/checkIsAuth";
+import { routes } from "@/shared/routes";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const isAuth = await checkIsAuth();
+
+  if (!isAuth) {
+    redirect(routes.signIn.url());
+  }
+
+  redirect(routes.dashboard.url());
+}

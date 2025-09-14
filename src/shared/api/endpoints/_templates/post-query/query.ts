cb6bb73c51e74a346094d488f318650ba3824f27
Vasyl M. <vasia.motnin@gmail.com>
2025-09-14 21:52:01 +0200

    Add createFolderQuery and schema for folder creation functionality; introduce new endpoint template for better API organization.

---
+++ src/shared/api/endpoints/_templates/post-query/query.ts
@@ -0,0 +1,27 @@
+import { fetchCustom, FetchCustomType } from "@/shared/api/core/fetchCustom";
+import * as v from "valibot";
+import { Fetchable } from "../../types/Fetchable";
+import { createFolderSchema } from "./schema";
+
+interface Args {
+  name: string;
+  parentFolderId?: string;
+}
+
+class CreateFolderQuery implements Fetchable {
+  constructor(private readonly fetchInstance: FetchCustomType) {}
+
+  private readonly _url = "folder/create";
+
+  async fetch(args: Args) {
+    const result = await this.fetchInstance(this._url, {
+      method: "POST",
+      body: args,
+    });
+    const data = await result.json();
+
+    return v.parse(createFolderSchema, data);
+  }
+}
+
+export const createFolderQuery = new CreateFolderQuery(fetchCustom);

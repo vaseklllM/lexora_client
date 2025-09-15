f6d1ca5065d24ec9a185cadd65d08b997cc7e3d4
Vasyl M. <vasia.motnin@gmail.com>
2025-09-15 12:13:39 +0200

    Refactor DropdownMenu component: replace button implementation with DropdownMenuComponent, enhance button management using useMemo for improved performance, and streamline modal handling for folder and deck creation.

--- src/widgets/section/ui/DropdownMenu/index.tsx
+++ src/widgets/section/ui/DropdownMenu/index.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { Plus } from "@/shared/icons/Plus";
-import { ReactElement, useId, useRef, useState } from "react";
+import {
+  DropdownItem,
+  DropdownMenu as DropdownMenuComponent,
+} from "@/entities/DropdownMenu";
+import { ReactElement, useMemo, useState } from "react";
 import { tv } from "tailwind-variants";
 import { ModalCreateDesc } from "./ModalCreateDesc";
 import { ModalCreateFolder } from "./ModalCreateFolder";
 
+const enum Button {
+  CREATE_FOLDER,
+  CREATE_DECK,
+}
+
 const classesSlots = tv({
   slots: {
     base: "",
-    button: "btn btn-primary h-10 w-10 rounded-full p-0 shadow-md",
-    list: "dropdown dropdown-end dropdown-top menu rounded-box bg-base-100 mb-2 shadow-sm",
   },
 });
 
@@ -19,53 +25,43 @@ interface Props {
 }
 
 export const DropdownMenu = (props: Props): ReactElement => {
-  const id = useId();
-  const popoverId = `popover-${id}`;
-  const anchorName = `--anchor-${id}`;
-  const popoverListRef = useRef<HTMLUListElement>(null);
-
   const classes = classesSlots();
   const [isOpenModalCreateFolder, setIsOpenModalCreateFolder] = useState(false);
   const [isOpenModalCreateDesc, setIsOpenModalCreateDesc] = useState(false);
 
+  const buttons = useMemo((): DropdownItem[] => {
+    return [
+      {
+        id: Button.CREATE_FOLDER,
+        type: "button",
+        label: "Folder",
+        // icon: "folder",
+        onClick: ({ closePopover }) => {
+          setIsOpenModalCreateFolder(true);
+          closePopover();
+        },
+      },
+      {
+        id: Button.CREATE_DECK,
+        type: "button",
+        label: "Deck",
+        // icon: "deck",
+        onClick: ({ closePopover }) => {
+          setIsOpenModalCreateDesc(true);
+          closePopover();
+        },
+      },
+    ];
+  }, []);
+
   return (
     <div className={classes.base({ className: props.className })}>
-      <button
-        className={classes.button()}
-        popoverTarget={popoverId}
-        style={{ anchorName: anchorName } as React.CSSProperties}
-      >
-        <Plus className="stroke-neutral-content" />
-      </button>
-
-      <ul
-        ref={popoverListRef}
-        className={classes.list()}
-        popover="auto"
-        id={popoverId}
-        style={{ positionAnchor: anchorName } as React.CSSProperties}
-      >
-        <li>
-          <button
-            onClick={() => {
-              setIsOpenModalCreateFolder(true);
-              popoverListRef.current?.hidePopover();
-            }}
-          >
-            Folder
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              setIsOpenModalCreateDesc(true);
-              popoverListRef.current?.hidePopover();
-            }}
-          >
-            Deck
-          </button>
-        </li>
-      </ul>
+      <DropdownMenuComponent
+        items={buttons}
+        buttonType="plus"
+        listPosition="end-top"
+        listClassName="mb-2"
+      />
       <ModalCreateFolder
         isOpen={isOpenModalCreateFolder}
         setIsOpen={setIsOpenModalCreateFolder}

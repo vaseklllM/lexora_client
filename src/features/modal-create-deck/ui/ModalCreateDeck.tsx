335ec65362fdf61947c746f250861c69124743ed
Vasyl M. <vasia.motnin@gmail.com>
2025-09-15 16:41:00 +0200

    Enhance ModalCreateDeck component: implement form validation for deck name using valibot, add focus management for input field on modal open, and update UI structure for improved user experience.

--- src/features/modal-create-deck/ui/ModalCreateDeck.tsx
+++ src/features/modal-create-deck/ui/ModalCreateDeck.tsx
@@ -1,5 +1,28 @@
-import { ReactElement, useCallback } from "react";
+import { InputLabeled } from "@/entities/input-labeled";
+import { MAX_DECK_NAME_LENGTH } from "@/shared/config";
+import { noOnlySpacesStringSchema } from "@/shared/schemas/noOnlySpacesString.schema";
+import { Button } from "@/shared/ui/Button";
+import { assignRef } from "@/shared/utils/assign-ref";
+import { valibotResolver } from "@/shared/utils/valibot-resolver";
+import { ReactElement, useCallback, useEffect, useRef } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { tv } from "tailwind-variants";
+import * as v from "valibot";
+
+const schema = v.object({
+  deck_name: v.pipe(
+    v.string(),
+    v.transform((input) => input.trim()),
+    v.nonEmpty("Name is required"),
+    noOnlySpacesStringSchema("Name cannot be only spaces"),
+    v.maxLength(
+      MAX_DECK_NAME_LENGTH,
+      `Name cannot be longer than ${MAX_DECK_NAME_LENGTH} characters`,
+    ),
+  ),
+});
+
+type Inputs = v.InferOutput<typeof schema>;
 
 const classesSlots = tv({
   slots: {
@@ -15,10 +38,50 @@ interface Props {
 
 export const ModalCreateDeck = (props: Props): ReactElement => {
   const classes = classesSlots();
+  const nameFieldRef = useRef<HTMLInputElement>(null);
+
+  const {
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    register,
+    setError,
+    reset,
+  } = useForm<Inputs>({
+    defaultValues: {
+      deck_name: "",
+    },
+    resolver: valibotResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = async () => {
+    try {
+      // console.log(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError("deck_name", { message: error.message });
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (props.isOpen) {
+      const timer = setTimeout(() => {
+        if (nameFieldRef.current) {
+          nameFieldRef.current.focus();
+        }
+      }, 100);
+
+      return () => clearTimeout(timer);
+    }
+  }, [props.isOpen]);
 
   const closeHandler = useCallback(() => {
     props.setIsOpen(false);
   }, [props.setIsOpen]);
+
+  const nameRegister = register("deck_name", {
+    required: true,
+  });
 
   return (
     <dialog
@@ -32,14 +95,38 @@ export const ModalCreateDeck = (props: Props): ReactElement => {
         <p className="text-base-content/70 mt-4">
           A deck is used to store word cards and study them
         </p>
-        <div className="modal-action">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button type="button" className="btn" onClick={closeHandler}>
-              Close
-            </button>
-          </form>
-        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="modal-action flex flex-col gap-4"
+        >
+          <InputLabeled
+            {...nameRegister}
+            ref={assignRef(nameRegister.ref, nameFieldRef)}
+            error={errors.deck_name?.message}
+            type="text"
+            autoComplete="name"
+            autoFocus={props.isOpen}
+            tabIndex={-1}
+            label="Name"
+          />
+
+          <div className="mt-4 flex w-full justify-end gap-4">
+            <Button
+              className="btn-soft"
+              type="button"
+              disabled={isSubmitting}
+              onClick={() => {
+                reset();
+                props.setIsOpen(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button className="btn-primary" isLoading={isSubmitting}>
+              Create
+            </Button>
+          </div>
+        </form>
       </div>
     </dialog>
   );
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -2,3 +2,8 @@
  * The maximum length of a folder name
  */
 export const MAX_FOLDER_NAME_LENGTH = 50;
+
+/**
+ * The maximum length of a deck name
+ */
+export const MAX_DECK_NAME_LENGTH = 50;

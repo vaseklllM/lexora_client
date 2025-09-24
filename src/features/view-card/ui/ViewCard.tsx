ddb6fa3de7650c52f52e9948a5ccdc120ade4046
Vasyl M. <vasia.motnin@gmail.com>
2025-09-24 12:54:23 +0200

    Refactor ViewCard component for improved layout and styling
    - Updated the ViewCard component to enhance the front view layout, including a title container and description display.
    - Introduced conditional styling for the front view based on the presence of descriptions, improving visual clarity.
    - Refactored class management to streamline the application of styles, ensuring a more cohesive design.

--- src/features/view-card/ui/ViewCard.tsx
+++ src/features/view-card/ui/ViewCard.tsx
@@ -5,10 +5,24 @@ import { tv } from "tailwind-variants";
 
 const classesSlots = tv({
   slots: {
-    card: "",
-    front: "flex h-full w-full items-center justify-center",
-    buttonAdd: "btn btn-dash btn-primary rounded-full font-light",
-    back: "flex h-full w-full flex-col justify-between",
+    base: "",
+    front: "flex h-full w-full flex-col items-center pt-10 pb-10",
+    frontTitleContainer: "",
+    frontTitle: "text-2xl font-bold",
+    frontTitleTranslation: "text-base-content/60 mt-2 text-base",
+    frontDescription: "text-base-content/90 text-sm",
+    back: "",
+    backTitle: "",
+  },
+  variants: {
+    isDescription: {
+      true: {
+        front: "justify-between",
+      },
+      false: {
+        front: "justify-center",
+      },
+    },
   },
 });
 
@@ -20,20 +34,37 @@ interface Props {
 export const ViewCard = memo((props: Props): ReactElement => {
   const [activeSide, setActiveSide] = useState<CardSide>("front");
 
-  const classes = classesSlots();
+  const classes = classesSlots({
+    isDescription:
+      !!props.card.descriptionInLearningLanguage ||
+      !!props.card.descriptionInKnownLanguage,
+  });
 
   return (
     <Card
-      className={classes.card({ className: props.className })}
+      className={classes.base({ className: props.className })}
       activeSide={activeSide}
       onSideChange={setActiveSide}
       front={
         <div className={classes.front()}>
-          {props.card.textInLearningLanguage}
+          <div className={classes.frontTitleContainer()}>
+            <h2 className={classes.frontTitle()}>
+              {props.card.textInLearningLanguage}
+            </h2>
+            <p className={classes.frontTitleTranslation()}>
+              ({props.card.textInKnownLanguage})
+            </p>
+          </div>
+          {props.card.descriptionInLearningLanguage && (
+            <p className={classes.frontDescription()}>
+              {props.card.descriptionInLearningLanguage}
+            </p>
+          )}
         </div>
       }
-      back={<div>back</div>}
-      hoverSwitch
+      back={
+        <div className={classes.back()}>{props.card.textInKnownLanguage}</div>
+      }
     />
   );
 });

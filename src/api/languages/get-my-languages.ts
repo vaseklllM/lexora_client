40015a0253a8486f1b9233a119c4dcd4a95c610c
Vasyl M. <vasia.motnin@gmail.com>
2025-09-24 23:06:41 +0200

    Add getMyLanguages API for fetching user language data
    - Introduced getMyLanguages function to retrieve languages known and learned by the user.
    - Implemented validation schema for the response data to ensure data integrity.
    - Added revalidateGetMyLanguages function to support cache revalidation for improved performance.

---
+++ src/api/languages/get-my-languages.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { fetchCustom } from "@/shared/api-core/fetchCustom";
+import { stackQueryKeys } from "@/shared/api-core/stackQueryKeys";
+import { revalidateTag } from "next/cache";
+import * as v from "valibot";
+import { languageSchema } from "../schemas/language.schema";
+
+const resultSchema = v.object({
+  languagesWhatIKnow: v.array(languageSchema),
+  languagesWhatILearn: v.array(languageSchema),
+});
+
+const tag = `languages-my__${stackQueryKeys.next()}`;
+
+export async function getMyLanguages() {
+  const result = await fetchCustom("languages/my", {
+    next: {
+      tags: [tag],
+    },
+  });
+
+  return v.parse(resultSchema, result.data);
+}
+
+export async function revalidateGetMyLanguages() {
+  revalidateTag(tag);
+}

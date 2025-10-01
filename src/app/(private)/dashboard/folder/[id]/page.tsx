e9a3db0be56f44f3f5b985867dbbec72e0716af3
Vasyl M. <vasia.motnin@gmail.com>
2025-10-01 16:10:12 +0200

    Add animated ball component to RootLayout and clean up commented code in dashboard pages

--- src/app/(private)/dashboard/folder/[id]/page.tsx
+++ src/app/(private)/dashboard/folder/[id]/page.tsx
@@ -1,8 +1,6 @@
 import { getFolder } from "@/api/folder/get-folder";
 import { getAllLanguages } from "@/api/languages/get-all-languages";
 import { getMyLanguages } from "@/api/languages/get-my-languages";
-// import { routes } from "@/shared/routes";
-// import Link from "next/link";
 import { Section } from "@/widgets/section";
 import { ReactElement } from "react";
 
@@ -18,20 +16,6 @@ export default async function FolderPage(props: Props): Promise<ReactElement> {
   const myLanguages = await getMyLanguages();
   const { childFolders, childDecks, breadcrumbs, parentFolder, ...folder } =
     await getFolder(params.id);
-
-  // return (
-  //   <div>
-  //     <h1>Home</h1>
-  //     <Link href={routes.dashboard.url()}>back</Link>
-  //     <div>
-  //       {childDecks.map((folder) => (
-  //         <Link href={routes.dashboard.folder.url(folder.id)} key={folder.id}>
-  //           {folder.name}
-  //         </Link>
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
 
   return (
     <Section
--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,51 +1,12 @@
 import { getDashboard } from "@/api/dashboard/get-dashboard";
 import { getAllLanguages } from "@/api/languages/get-all-languages";
 import { getMyLanguages } from "@/api/languages/get-my-languages";
-// import { routes } from "@/shared/routes";
-// import { Link } from "@/shared/ui/Link";
 import { Section } from "@/widgets/section";
 
 export default async function Home() {
   const dashboardData = await getDashboard();
   const languages = await getAllLanguages();
   const myLanguages = await getMyLanguages();
-
-  //   return (
-  //     <div>
-  //       <h1>Home</h1>
-  //       <div>
-  //         {dashboardData.childFolders.map((folder) => (
-  //           <Link href={routes.dashboard.folder.url(folder.id)} key={folder.id}>
-  //             {folder.name}
-  //           </Link>
-  //         ))}
-  //       </div>
-  //       <div>
-  //         <div className="ball-holder">
-  //           <div className="ball"></div>
-  //         </div>
-  //         <style>
-  //           {`
-  //             .ball {
-  //   width: 80px;
-  //   height: 80px;
-  //   border-radius: 50%;
-  //   background: linear-gradient(teal, #000);
-  // }
-
-  // .ball {
-  //   display: block;
-  //   transition: all 2s;
-  // }
-
-  // .ball-holder:hover .ball {
-  //   transform: translateX(calc(100vw - 80px)) rotate(360deg);
-  // }
-  //           `}
-  //         </style>
-  //       </div>
-  //     </div>
-  //   );
 
   return (
     <Section

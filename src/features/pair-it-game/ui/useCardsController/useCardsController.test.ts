a66b7af0a57f0342aff8d16970a0745c4a8a84e4
Vasyl M. <vasia.motnin@gmail.com>
2025-10-10 11:20:51 +0200

    Add tests for additional card scenarios in useCardsController

--- src/features/pair-it-game/ui/useCardsController/useCardsController.test.ts
+++ src/features/pair-it-game/ui/useCardsController/useCardsController.test.ts
@@ -1,4 +1,4 @@
-import { card1 } from "@/shared/test/cards";
+import { card1, card2, card3, card4, card5, card6 } from "@/shared/test/cards";
 import { renderHook } from "@testing-library/react";
 import { useCardsController } from "./useCardsController";
 
@@ -15,5 +15,19 @@ describe("useCardsController", () => {
   it("should return one card", () => {
     const { result } = renderHook(() => useCardsController([card1]));
     expect(result.current.cards).toEqual([card1]);
+  });
+
+  it("should return all five cards", () => {
+    const { result } = renderHook(() =>
+      useCardsController([card1, card2, card3, card4, card5]),
+    );
+    expect(result.current.cards).toEqual([card1, card2, card3, card4, card5]);
+  });
+
+  it("should return first five cards", () => {
+    const { result } = renderHook(() =>
+      useCardsController([card1, card2, card3, card4, card5, card6]),
+    );
+    expect(result.current.cards).toEqual([card1, card2, card3, card4, card5]);
   });
 });

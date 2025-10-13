c220b5cdc16dbb51c12cdb3be6a005760d664194
Vasyl M. <vasia.motnin@gmail.com>
2025-10-13 21:16:46 +0200

    Add Input component with styling and functionality
    - Introduced a new Input component with customizable styles and error handling.
    - Added CSS for input field transitions and visual feedback for OnePassword integration.
    - Implemented password visibility toggle feature using an eye icon for enhanced user experience.

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,12 +1,13 @@
 import React, { InputHTMLAttributes, useState } from "react";
 import { tv } from "tailwind-variants";
-import { EyeIcon } from "../icons/Eye";
+import { EyeIcon } from "../../icons/Eye";
+import "./Input.css";
 
 const classesSlots = tv({
   slots: {
     base: "relative",
     inputField:
-      "block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-gray-800 dark:text-gray-100 dark:outline-gray-700 dark:placeholder:text-gray-500",
+      "block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 data-[com-onepassword-filled=light]:bg-gray-800! sm:text-sm/6 dark:bg-gray-800 dark:text-gray-100 dark:outline-gray-700 dark:placeholder:text-gray-500",
     errorMessage: "mt-1 text-left text-xs text-red-500",
     eyeButton:
       "btn text-base-content/70 btn-ghost btn-sm btn-circle absolute top-[2px] right-[2px]",

198c46e84d682dec90d47ab50f204516ff61ce1c
Vasyl M. <vasia.motnin@gmail.com>
2025-10-17 11:13:43 +0200

    Refactor next.config.ts and add console log in UserIcon component
    - Removed unnecessary pathname entry for Google image hosts in next.config.ts to streamline configuration.
    - Added a console log statement in the UserIcon component to assist with debugging props, enhancing development visibility.

--- next.config.ts
+++ next.config.ts
@@ -16,13 +16,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        pathname: "/**",
       },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "lh4.googleusercontent.com" },
       { protocol: "https", hostname: "lh5.googleusercontent.com" },
       { protocol: "https", hostname: "lh6.googleusercontent.com" },
-      { protocol: "https", hostname: "profiles.google.com" },
     ],
   },
 };

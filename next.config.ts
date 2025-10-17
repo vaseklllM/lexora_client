da492194c53627f6c2f6939b4047135f50138afa
Vasyl M. <vasia.motnin@gmail.com>
2025-10-17 11:24:58 +0200

    Refactor image host configuration in next.config.ts
    - Streamlined the image host configuration by removing unnecessary pathname entries for Google image hosts, simplifying the setup.
    - Retained essential entries for lh3, lh4, lh5, and lh6 Google subdomains to ensure continued image loading functionality.

--- next.config.ts
+++ next.config.ts
@@ -5,18 +5,9 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.join(__dirname, ".."),
   },
-  /* config options here */
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn2.thecatapi.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "lh4.googleusercontent.com" },
       { protocol: "https", hostname: "lh5.googleusercontent.com" },
       { protocol: "https", hostname: "lh6.googleusercontent.com" },
